3165cad7b925424bd3a8edc4339c2857dfab5881
crvernon <chris.vernon@pnnl.gov>
2020-03-29 20:01:35 -0400

    add skip counting function

--- kids_math/utils.py
+++ kids_math/utils.py
@@ -6,8 +6,21 @@
 def say_hello(name):
     """Print out a greeting.
 
-    :param name:        String for person's name
+    :param name:                            string;  person's name
 
     """
     print("Hello {}!".format(name))
+
+
+def skip_counting(start_number, through_number, by_number):
+    """Skip count.
+
+    :param start_number:                    integer;  start number
+    :param through_number:                  integer;  number to count to
+    :param by_number:                       integer;  number to count by
+
+    """
+
+    for i in range(start_number, through_number + by_number, start_number):
+        print(i)
 
