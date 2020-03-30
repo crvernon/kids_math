fd40f4fdf4bc206ece7e3a70654843bc4bbed1cf
crvernon <chris.vernon@pnnl.gov>
2020-03-29 20:38:07 -0400

    adding greater than game

--- kids_math/utils.py
+++ kids_math/utils.py
@@ -1,6 +1,25 @@
-"""Common utility functions for the `kids_math` package
+"""Common utility functions for the `kids_math` package"""
 
-"""
+
+def valid_answer(response, acceptable, statement):
+    """Check value and iterate until valid or too many attempts.
+
+    :param response:                        response from user
+    :param acceptable:                      tuple or list; acceptable values
+    :param statement:                       string; response to user
+
+    """
+    valid = 0
+    while valid < 5:
+        if response not in acceptable:
+            response = input(statement)
+            valid += 1
+        else:
+            return response
+
+    if response not in acceptable:
+        print('Sorry still not the correct input.  Ask someone for help and try again.')
+        raise ValueError
 
 
 def say_hello(name):
